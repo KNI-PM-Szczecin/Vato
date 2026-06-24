2e64ed79821a0ee89447214ccbb705572142d104
DudeQ <dutkiewicz.paw@tutamail.com>
2026-06-24 10:00:10 +0200

    adjusted and corrected githubaction dependcies

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import os
 import sys
 from dotenv import load_dotenv
 
+try:
+    import env_vars  
+except ImportError:
+    pass             
+
 if getattr(sys, 'frozen', False):
     base_path = sys._MEIPASS
 else:
     base_path = os.path.dirname(os.path.abspath(__file__))
 
-load_dotenv(dotenv_path=os.path.join(base_path, '.env'))
+load_dotenv(dotenv_path=os.path.join(base_path, '.env')) 
 
 from app import App
 
